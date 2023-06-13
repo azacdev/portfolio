c20a3cb07b11edc554ee8a3e10601655b086bda2
azacdev <azacdev@gmail.com>
2023-06-13 02:11:59 +0100

    update

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,21 +1,37 @@
-import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa"
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { useContext } from "react";
+import { ThemeContext } from "../themeContext/ThemeProvider";
 
 const Social = () => {
+  const { theme } = useContext(ThemeContext);
+
   return (
     <div className="home__social">
-        <a href="https://github.com/azacdev" className="home__social-icon" target="blank">
-          <FaGithub/>
-        </a>
+      <a
+        href="https://github.com/azacdev"
+        className={`${theme ? "text-color-light" : "text-color-dark"} github`}
+        target="blank"
+      >
+        <FaGithub />
+      </a>
 
-        <a href="https://linkedin.com/in/azacdev/" className="home__social-icon" target="blank">
-          <FaLinkedin/>
-        </a>
+      <a
+        href="https://linkedin.com/in/azacdev/"
+        className={`${theme ? "text-color-light" : "text-color-dark"} linkedin`}
+        target="blank"
+      >
+        <FaLinkedin />
+      </a>
 
-        <a href="https://twitter.com/azacdev" className="home__social-icon" target="blank">
-          <FaTwitter/>
-        </a>
+      <a
+        href="https://twitter.com/azacdev"
+        className={`${theme ? "text-color-light" : "text-color-dark"} twitter`}
+        target="blank"
+      >
+        <FaTwitter />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default Social
+export default Social;
