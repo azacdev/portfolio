c8a19d09df6872474319b6025d8ebe199815a782
azacdev <azacdev@gmail.com>
2023-06-15 23:33:47 +0100

    update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,33 +1,57 @@
-import React from "react";
+import { useContext } from "react";
+import { ThemeContext } from "../themeContext/ThemeProvider";
+import Social from "../home/Social";
+
 import "./Footer.css";
 
 const Footer = () => {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <footer className="footer">
+    <footer
+      className={` ${
+        theme ? "bg-light text-color-light" : "bg-dark text-color-dark"
+      } footer`}
+    >
       <div className="footer__container container">
         <h1 className="footer__title">Abdulrazak</h1>
 
         <ul className="footer__list">
           <li>
-            <a href="#about" className="footer__link">
+            <a
+              href="#about"
+              className={`${
+                theme ? "text-color-light" : "text-color-dark"
+              } footer__link`}
+            >
               About
             </a>
           </li>
 
           <li>
-            <a href="#skills" className="footer__link">
+            <a
+              href="#skills"
+              className={`${
+                theme ? "text-color-light" : "text-color-dark"
+              } footer__link`}
+            >
               Skills
             </a>
           </li>
 
           <li>
-            <a href="#projects" className="footer__link">
+            <a
+              href="#projects"
+              className={`${
+                theme ? "text-color-light" : "text-color-dark"
+              } footer__link`}
+            >
               Projects
             </a>
           </li>
         </ul>
 
-        <div className="footer__social">
+        {/* <div className="footer__social">
           <a
             href="https://twitter.com/azacdev"
             className="footer__social-link"
@@ -51,7 +75,9 @@ const Footer = () => {
           >
             <i class="bx bxl-facebook"></i>
           </a>
-        </div>
+        </div> */}
+
+        <Social/>
 
         <span className="footer__copy">
           &#169; Azacdev. All rigths reserved
