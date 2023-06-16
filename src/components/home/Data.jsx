dab8747a27b7c2ebe84d47ab2e240b1f29797825
azacdev <azacdev@gmail.com>
2023-06-16 16:14:08 +0100

    update

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -1,9 +1,30 @@
-import { useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import { ThemeContext } from "../themeContext/ThemeProvider";
 import { LuSend } from "react-icons/lu";
 
 const Data = () => {
   const { theme } = useContext(ThemeContext);
+
+  const [text, setText] = useState("");
+  const message = "> H'i, I'm Abdulrazak";
+
+  useEffect(() => {
+    let currentText = "";
+    let currentIndex = 0;
+
+    const typingInterval = setInterval(() => {
+      if (currentIndex === message.length) {
+        clearInterval(typingInterval);
+        return;
+      }
+
+      currentText += message[currentIndex];
+      setText(currentText);
+      currentIndex++;
+    }, 100);
+
+    return () => clearInterval(typingInterval);
+  }, []);
 
   return (
     <div className="home__data">
@@ -13,7 +34,7 @@ const Data = () => {
             theme ? "text-color-light" : "text-color-dark"
           } home__title`}
         >
-          {`> H'i, I'm Abdulrazak`}
+          {text}
         </h1>
         <h3
           className={`${
