b29f9ea7986720750af64a7840a7092f28f4a371
azacdev <azacdev@gmail.com>
2024-01-29 22:48:59 +0100

    test: vercel speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "@/styles/globals.css";
 import Navbar from "@/components/navbar";
@@ -36,6 +37,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+        <SpeedInsights />
       </body>
     </html>
   );
