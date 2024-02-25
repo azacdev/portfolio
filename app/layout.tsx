8d86ac3e79e0ebfdba41bd08fc70bc9b04c9f6cb
azacdev <azacdev@gmail.com>
2024-02-25 01:41:23 +0100

    chore: opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,8 @@ import Script from "next/script";
 const montserrat = Montserrat({ subsets: ["latin"], variable: "--font-mint" });
 
 export const metadata: Metadata = {
+  // @ts-ignore
+  metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL),
   title: {
     default: "Abdulrazak Abubakar (Azacdev) Full Stack Web & Mobile Developer",
     template:
@@ -18,8 +20,32 @@ export const metadata: Metadata = {
   },
   description:
     "Experienced Software Developer specializing in web and mobile development. Proficient in React, React Native, and responsive design. Let's build innovative solutions together.",
+  keywords: [
+    "web dev",
+    "web developer kano",
+    "web developer abuja",
+    "web developer lagos",
+    "web developer nigeria",
+    "azacdev",
+    "abdulrazak abubakar",
+  ],
   twitter: {
     card: "summary_large_image",
+    title: "Azacdev",
+    images: "./opengraph-image.webp",
+    description:
+      "Experienced Software Developer specializing in web and mobile development. Proficient in React, React Native, and responsive design. Let's build innovative solutions together.",
+    creator: "azacdev",
+  },
+  openGraph: {
+    type: "website",
+    url: "https://www.azacdev.com",
+    locale: "en_US",
+    title: "Azacdev",
+    images: "./opengraph-image.webp",
+    description:
+      "Experienced Software Developer specializing in web and mobile development. Proficient in React, React Native, and responsive design. Let's build innovative solutions together.",
+    siteName: "Azacdev",
   },
 };
 
