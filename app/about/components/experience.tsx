05628a25a047550dfdefb5fd7516e6918360fc99
azacdev <azacdev@gmail.com>
2024-03-30 00:15:00 +0100

    chore

--- app/about/components/experience.tsx
+++ app/about/components/experience.tsx
@@ -62,35 +62,37 @@ const Experience = () => {
 
         <ul className="w-full flex flex-col items-center justify-between ml-2 sm:ml-4">
           <Details
-            position="Frontend Developer"
+            position="Intern Frontend Developer"
             company="Engausa"
-            time="2021-2022"
-            address=" Kano, Kano"
-            work="In my role as a web developer, I crafted websites using the classic trio of HTML, CSS, and JavaScript. I played a key role in weaving together visually appealing and interactive web experiences, adding a touch of creativity to the digital landscape."
+            time="Jan/2021 - Jun/2022"
+            address="Kano, Nigeria"
+            work="In my role as a front-end developer i developed responsive and cross-browser compatible front-end code for high-traffic websites proactively researched 3rd party libraries to avoid technical debt and refactored code to adhere to best practices, engaged in code reviews and provided constructive feedback to enhance team productivity"
           />
 
           <Details
             position="Freelance Developer"
             company="Freelance"
-            time="2022-2023"
+            time="Jun/2022 - June/2023"
             address="Remote"
-            work="During my freelance endeavors, I specialized in crafting dynamic web applications using cutting-edge technologies like React, Redux, and Firebase. I thrived in bringing clients' visions to life, delivering responsive and feature-rich solutions that leverage the power of modern front-end frameworks and real-time databases."
+            work="During my freelance endeavors, I specialized in crafting dynamic web applications using cutting-edge technologies like React, Redux, and Firebase, delivered high-quality, SEO-optimized front-end code for web applications, meeting client specifications and 
+            exceeding expectations, designed sleek and functional websites that ensured optimal user experience and client satisfaction."
           />
 
           <Details
-            position="Web Developer"
+            position="Full Stack Developer"
             company="Office Box"
-            time="2023-2023"
-            address="Kano, Kano"
-            work="In my recent at role at Box, I took charge of the frontend development using Next.js, TypeScript, Tailwind CSS, and Shadcn UI in the creation of Ribbons, an innovative app enabling anonymous reporting of rape cases. Additionally, I played a pivotal role in the development of AgroSmart Tech, a platform facilitating seamless connections between buyers and sellers in the agricultural domain."
+            time="Jun/2023 - Dec/2023"
+            address="Kano, Nigeria"
+            work="In my recent at role at office Box,i contributed to the development of a pioneering web app for anonymous reporting of rape cases, featuring location based assistance and a streamlined dashboard for efficient case management by authorities, collaborated on an agro-based project linking farmers with investors, integrating investment tracking by crop and implementing a dashboard with wallet functionality for tracking produce and finance"
           />
 
           <Details
-            position="Freelance Developer"
-            company="Freelance"
-            time="2023-Present"
+            position="Frontend Developer"
+            company="Talents Sphere"
+            time="Feb/2024 - Present"
             address="Remote"
-            work="Dedicated to full-stack development, I excel in developing diverse web experiences, encompassing dynamic applications, dashboards, CMS, documentation pages, and sleek landing pages. My toolkit, featuring Next.js, Node.js, Express, MongoDB, Tailwind CSS, Redux, and React Native, ensures a cohesive and engaging user journey across varied projects."
+            work="Engineered and optimized front-end authentication systems, achieving a 40% reduction in user registration time 
+            and enhancing platform security for Talent Sphere Africa's automated talent-matching system, transformed Figma designs into responsive frontend interfaces using modern web technologies, enhancing user engagement and enabling seamless connections across diverse African regions."
           />
         </ul>
       </div>
