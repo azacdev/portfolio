d694a05acdefa00769d31a2b0422b32c4ecc881c
azacdev <azacdev@gmail.com>
2024-08-10 22:57:49 +0100

    fix: sitemap

--- next.config.js
+++ next.config.js
@@ -7,7 +7,16 @@ module.exports = withBundleAnalyzer({});
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
+    ],
   },
 };
 
